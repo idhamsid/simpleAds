2c1b33ace51fa08fa1601e0a2495647aee78e808
idhamsid <idhams@gmail.com>
2022-01-30 21:11:56 +0700

    Initial commit

---
+++ app/src/main/java/com/aliendroid/samplealienads/SettingAds.java
@@ -0,0 +1,51 @@
+package com.aliendroid.samplealienads;
+
+public class SettingAds {
+    public static String SELECT_ADS = "APPLOVIN-M";
+    public static String BACKUP_ADS = "APPLOVIN-D";
+
+    public static String MAIN_ADS_BANNER = "ca-app-pub-3940256099942544/6300978111x";
+    public static String BACKUP_ADS_BANNER = "DefaultBanner";
+
+    public static String MAIN_ADS_INTERTITIAL = "ca-app-pub-3940256099942544/1033173712x";
+    public static String BACKUP_ADS_INTERTITIAL = "Game_Screen";
+
+    public static String MAIN_ADS_REWARDS = "ca-app-pub-3940256099942544/5224354917x";
+    public static String BACKUP_ADS_REWARDS = "DefaultRewardedVideo";
+
+    public static String OPEN_ADS_ADMOB = "ca-app-pub-3940256099942544/3419835294x";
+    public static String NATIVE_ADS_ADMOB = "/6499/example/native";
+    public static String INITIALIZE_SDK = "1234567";
+    public static String INITIALIZE_SDK_BACKUPADS = "107355779";
+
+    public static String HIGH_PAYING_KEYWORD1 = "Finance";
+    public static String HIGH_PAYING_KEYWORD2 = "";
+    public static String HIGH_PAYING_KEYWORD3 = "";
+    public static String HIGH_PAYING_KEYWORD4 = "";
+    public static String HIGH_PAYING_KEYWORD5 = "";
+
+    public static int INTERVAL = 0;
+
+
+    public static String ADMOB_INTER = "ca-app-pub-3940256099942544/1033173712";
+    public static String ADMOB_BANNER = "ca-app-pub-3940256099942544/6300978111";
+    public static String ADMOB_OPEN = "ca-app-pub-3940256099942544/3419835294";
+
+    public static String UNITY_GAME_ID = "3916229";
+    public static String UNITY_BANNER = "banner";
+    public static String UNITY_INTERSTITIAL = "Android_Interstitial";
+    public static String UNITY_REWARD = "Android_Rewarded";
+
+    public static String DZONE_BANNER = "01180f81f16a1876";
+    public static String DZONE_INTERS = "23be86543d6d7f2f";
+
+    public static String MAX_BANNER = "ae44996e2b723dcf";
+    public static String MAX_INTERS = "2838649e0b560a68";
+    public static String MAX_NATIVE = "29d0d18da450ae9c";
+
+
+    public static String FAN_BANNER = "548194876497884_548195199831185";
+    public static String FAN_INTERS = "548194876497884_548228429827862";
+
+
+}
